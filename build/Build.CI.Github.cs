6980dddec457cb5101212f643302c81b9ff240e8
Daniel Jašek <jasekdan@gmail.com>
2022-04-22 00:05:08 +0200

    Added nuke build

---
+++ build/Build.CI.Github.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitHub;
+using Nuke.Common.Utilities.Collections;
+using Nuke.GitHub;
+using Serilog;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.GitHub.GitHubTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+[GitHubActions("release-main", GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new[] { "master", "main" },
+    InvokedTargets = new[] { nameof(PublishGithubRelease), nameof(Push) },
+    PublishArtifacts = true)]
+public partial class Build
+{
+    [Parameter]
+    readonly bool AutoStash = true;
+
+    string MajorMinorPatchVersion => GitVersion.MajorMinorPatch;
+    AbsolutePath PackagesDirectory => ArtifactsDirectory / "packages";
+
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .Apply(PackSettings));
+
+            ReportSummary(_ => _
+                .AddPair("Packages", PackagesDirectory.GlobFiles(".nupkg").Count.ToString()));
+        });
+
+    Target Push => _ => _
+        .DependsOn(Pack)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Executes(() =>
+        {
+            Log.Information("Running push to packages directory.");
+            
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+                .Where(x => !x.EndsWith("symbols.nupkg"))
+                .ForEach(x =>
+                {
+                    DotNetNuGetPush(s => s
+                        .SetTargetPath(x)
+                        .SetSource(GitRepository.HttpsUrl)
+                    );
+                });
+
+        });
+
+    Target PublishGithubRelease => _ => _
+        .DependsOn(Pack)
+        .OnlyWhenDynamic(() => GitRepository.IsOnMainOrMasterBranch())
+        .Executes<Task>(async () =>
+        {
+            Log.Information("Started creating release.");
+            var releaseTag = $"v{GitVersion.MajorMinorPatch}";
+
+            var githubOwner = GitRepository.GetGitHubOwner();
+            var repositoryName = GitRepository.GetGitHubName();
+
+            var nugetPackages = ArtifactsDirectory.GlobFiles("*.nupkg")
+                .Select(x => x.ToString()).ToArray();
+
+            await PublishRelease(conf => conf
+                .SetArtifactPaths(nugetPackages)
+                .SetCommitSha(GitVersion.Sha)
+                .SetRepositoryName(repositoryName)
+                .SetRepositoryOwner(githubOwner)
+                .SetTag(releaseTag)
+                .DisablePrerelease());
+        });
+
+
+    private Configure<DotNetPackSettings> PackSettings => _ => _
+        .SetProject(Solution)
+        .SetConfiguration(Configuration.Release)
+        .SetNoBuild(SucceededTargets.Contains(Compile))
+        .SetOutputDirectory(PackagesDirectory)
+        .When(GitRepository.HttpsUrl != null, (_) => _.SetRepositoryUrl(GitRepository.HttpsUrl));
+}
---
+++ build/Build.Tests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+
+public partial class Build
+{
+    AbsolutePath TestResultDirectory => ArtifactsDirectory / "test-results";
+
+    IEnumerable<Project> TestProjects => Partition.GetCurrent(Solution.GetProjects("*.Tests"));
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Produces(TestResultDirectory / "*.trx")
+        .Produces(TestResultDirectory / "*.xml")
+        .Executes(() =>
+        {
+            try
+            {
+                DotNetTest(_ => _
+                        .Apply(TestSettings)
+                        .CombineWith(TestProjects, (_, v) => _
+                            .Apply(TestProjectSettings, v)
+                            .Apply(TestProjectSettings, v)),
+                    completeOnFailure: true);
+            }
+            finally
+            {
+                ReportTestCount();
+            }
+        });
+
+    Configure<DotNetTestSettings> TestSettings => _ => _
+        .SetConfiguration(Configuration)
+        .SetNoBuild(SucceededTargets.Contains(Compile))
+        .ResetVerbosity()
+        .SetResultsDirectory(TestResultDirectory)
+        .When(IsServerBuild, _ => _
+            .EnableCollectCoverage()
+            .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+            .SetExcludeByFile("*.Generated.cs")
+            .When(IsServerBuild, _ => _
+                .EnableUseSourceLink()));
+
+    Configure<DotNetTestSettings, Project> TestProjectSettings => (_, v) => _
+        .SetProjectFile(v)
+        .SetLoggers($"trx;LogFileName={v.Name}.trx")
+        .When(IsServerBuild, _ => _
+            .SetCoverletOutput(TestResultDirectory / $"{v.Name}.xml"));
+
+    void ReportTestCount()
+    {
+        IEnumerable<string> GetOutcomes(AbsolutePath file)
+        {
+            return XmlTasks.XmlPeek(
+                file,
+                "/xn:TestRun/xn:Results/xn:UnitTestResult/@outcome",
+                ("xn", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010"));
+        }
+
+        var resultFiles = TestResultDirectory.GlobFiles("*.trx");
+        var outcomes = resultFiles.SelectMany(GetOutcomes).ToList();
+        var passedTests = outcomes.Count(x => x == "Passed");
+        var failedTests = outcomes.Count(x => x == "Failed");
+        var skippedTests = outcomes.Count(x => x == "NotExecuted");
+
+        ReportSummary(_ => _
+            .When(failedTests > 0, _ => _
+                .AddPair("Failed", failedTests.ToString()))
+            .AddPair("Passed", passedTests.ToString())
+            .When(skippedTests > 0, _ => _
+                .AddPair("Skipped", skippedTests.ToString())));
+    }
+}
